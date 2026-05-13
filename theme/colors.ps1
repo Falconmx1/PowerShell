b3ee4016851442fe85eeecae16f26a9c13a3edb9
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-05-12 20:23:54 -0600

    Create colors.ps1

---
+++ theme/colors.ps1
@@ -0,0 +1,9 @@
+# Colores RGB ANSI para PowerMaxShell
+$global:PMS_Red = "`e[38;2;255;85;85m"
+$global:PMS_Orange = "`e[38;2;255;170;85m"
+$global:PMS_Yellow = "`e[38;2;255;255;85m"
+$global:PMS_Green = "`e[38;2;85;255;85m"
+$global:PMS_Cyan = "`e[38;2;85;255;255m"
+$global:PMS_Blue = "`e[38;2;85;85;255m"
+$global:PMS_Purple = "`e[38;2;200;85;255m"
+$global:PMS_Reset = "`e[0m"
