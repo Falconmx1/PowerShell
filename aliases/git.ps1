106321ce7ab1cc296956613c0004406ce2dc2cf2
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-05-12 20:25:52 -0600

    Create git.ps1

---
+++ aliases/git.ps1
@@ -0,0 +1,16 @@
+# Alias estilo Git (Zsh style)
+Set-Alias gst   git status
+Set-Alias gco   git checkout
+Set-Alias gcb   git checkout -b
+Set-Alias gaa   git add --all
+Set-Alias gcm   git commit -m
+Set-Alias gcam  git commit -am
+Set-Alias glog  git log --oneline --graph
+Set-Alias gp    git push
+Set-Alias gpl   git pull
+Set-Alias gd    git diff
+Set-Alias gds   git diff --staged
+Set-Alias gb    git branch
+Set-Alias gba   git branch -a
+Set-Alias gcl   git clone
+Set-Alias grs   git reset --soft HEAD~1
